32a09166b6fb9166a6af8e5358afa7bfbb558d15
Shaine O'Neal <shaineoneal@gmail.com>
2026-07-02 17:33:53 -0500

    fix: url changed for user profile and fixed 404

--- src/account_scraper.py
+++ src/account_scraper.py
@@ -35,7 +35,7 @@ class LiveJournalAccount:
         self.base_url = f"https://{clean_username}.livejournal.com"
         self.urls = {
             "entries": self.base_url,
-            "profile": f"https://www.livejournal.com/{clean_username}/profile",
+            "profile": f"https://www.users.livejournal.com/{username}/profile",
             "tags": f"{self.base_url}{URL_SUFFIX['tags']}",
             "userpics": f"https://www.livejournal.com/allpics.bml?user={clean_username}",
             "vgifts": f"https://www.livejournal.com/manage/vgift.bml?u={clean_username}",
